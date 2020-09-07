260a303c9542b13940229d48da577b4cb959536e
hoohoo0889 <soehd0889@naver.com>
2020-09-07 19:45:18 +0900

    기능 추가

--- Make_web/GooGooDan/GooGooDan.js
+++ Make_web/GooGooDan/GooGooDan.js
@@ -4,8 +4,17 @@ function input(){
     num = input;
 }
 function GooGoodan(){
-    for(var i=1;i<=9;i++)
-    {
-        document.write(num + "x" + i + "=" + num*i + "<br>");
+    input();
+    if(num > 9){
+        alert('구구단은 9단까지입니다.');
+        document.getElementById("Input_Bar").value = null;
+    }
+    else{
+        document.write('<link rel="stylesheet" type="text/css" href="./GooGooDan.css">');
+        document.write('<p class="GooGoodan_text">' + num + "단" + "</p>" +"<br>");
+        for(var i=1;i<=9;i++)
+        {
+            document.write('<p class="GooGoodan_text">'+ num + "x" + i + "=" + num*i + "</p>" + "<br>");
+        }
     }
 }
