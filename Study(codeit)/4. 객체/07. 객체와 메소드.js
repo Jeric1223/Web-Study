a9ab03b76c1b2afd8f753d77891eb4410f6b0ea2
JaeHyun <soehd0889@naver.com>
2020-10-28 17:20:37 +0900

    10-28 07.객체와 메소드

---
+++ Study(codeit)/4. 객체/07. 객체와 메소드.js
@@ -0,0 +1,13 @@
+//메소드 
+let greetings = {
+  sayName: function(name) {
+    console.log(`${name}`);
+  },
+  sayHi: function() {
+    console.log('Hi!');
+  },
+  sayBye: function() {
+    console.log('Bye!');
+  }
+};
+greetings.sayName('김재현');
