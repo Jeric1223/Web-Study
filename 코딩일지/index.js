c976437e212f831b6833a825975f6e4d9a769d47
JaeHyun <soehd0889@naver.com>
2020-11-03 22:38:00 +0900

    11-03 SettingBox슬라이드 토클추가, 객체이용

--- 코딩일지/index.js
+++ 코딩일지/index.js
@@ -8,7 +8,6 @@ $('#menuButton').on('click',function menuLi() {
   });
 /* 모달창 닫기버튼 */
 function closeButton() {
-
   $('.modals').fadeOut();
   document.getElementById('modalInput').value = null;
   document.getElementById('modalTitleInput').value = null;
@@ -23,22 +22,56 @@ let year = today.getFullYear(); // 년도
 let month = today.getMonth() + 1;  // 월
 let date = today.getDate();  // 날짜
 
+
+
 dir.addEventListener('click', function() {
   let mainText = document.getElementById('modalInput').value;
   let Title = document.getElementById('modalTitleInput').value;
+
+  let writeObject = {
+    writeTitle: Title,
+    writeMainText: mainText
+   };
+   console.log(writeObject)
+
   mainText = mainText.replace(/(?:\r\n|\r\n)/g, '<br />');
 
   const listTemplate = `
   <div class="writeListBox" cursor: pointer;">
-  <div class="writeListBoxHeader">
-    <a href="#">${Title}</a>
-    <img src="./free-icon-vertical-dots-64576.png" style="width: 16px; height: 16px;">
+    <div class="writeListBoxHeader">
+      <a href="#">${writeObject.writeTitle}</a>
+      <p>${writeObject.writeMainText}</p>
+    </div>
+    <div class="writeListBoxSetting">
+      <img src="./free-icon-vertical-dots-64576.png" id="settingImage" style="width: 16px; height: 16px;">
+      <div class="settingBox">
+        <div><p>수정하기</p></div>
+        <div><p>삭제하기</p></div>
+    </div>
+    <p>일시 : ${year}-${month}-${date}</p>
   </div>
-  <p>${mainText}</p>
-  <p>일시 : ${year}-${month}-${date}</p>
 </div>
   `
+
+
   center.insertAdjacentHTML('afterbegin', listTemplate);
 
   closeButton();
+  //점 세개인 이미지를 클릭했을때 settingBox가 나타나는
+   $('#settingImage').on('click',function()
+  {
+   $('.settingBox').slideToggle();
+    console.log(102)
+  }); 
+  
+
+
 });
+
+//점 세개인 이미지를 클릭했을때 settingBox가 나타나는
+/* $('#settingImage').on('click',function()
+{
+  $('.settingBox').slideToggle();
+  console.log(102)
+}); */
+
