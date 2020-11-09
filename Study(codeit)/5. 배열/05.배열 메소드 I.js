925192e74fb3a912459f8df02ad461d780f1655d
JaeHyun <soehd0889@naver.com>
2020-11-09 20:44:09 +0900

    11-09 05.배열 메소드 I

---
+++ Study(codeit)/5. 배열/05.배열 메소드 I.js
@@ -0,0 +1,8 @@
+//배열의 메소드
+let members = [ '쿤갈레', 'Zerrard66', '우리생각했서',
+'흙토끼', 'END MIRACLE'];
+console.log(members);
+
+//splice(startIndex, deleteCount, item)
+members.splice(1,0, 'NiceCodeit','HiCodeit');
+console.log(members);
