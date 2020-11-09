f812ac1e2eb71d0774d75f98249830b25aeb0eab
JaeHyun <soehd0889@naver.com>
2020-11-09 20:32:13 +0900

    11-09 03.배열 다루기

---
+++ Study(codeit)/5. 배열/03.배열 다루기.js
@@ -0,0 +1,21 @@
+//배열
+
+let members = [ '쿤갈레', 'Zerrard66', '우리생각했서',
+'흙토끼', 'END MIRACLE'];
+
+console.log(typeof members);
+
+console.log(members.length);
+
+console.log(members['length']);
+
+console.log(members[members.length - 1]);
+
+members[6] = 'NiceCodeit';
+
+console.log(members[5]); // undefined
+
+console.log(members);
+
+
+
