e6ec129b93fbddd0c0fa9b90fb72b68064aa6b25
JaeHyun <soehd0889@naver.com>
2020-11-10 22:47:38 +0900

    11-10 07. 배열 메소드 II

---
+++ Study(codeit)/5. 배열/07. 배열 메소드 II.js
@@ -0,0 +1,17 @@
+// 배열의 메소드 
+let members = [ '쿤갈레', 'Zerrard66', '우리생각했서',
+'흙토끼', 'END MIRACLE'];
+console.log(members);
+
+//배열의 첫 요소를 삭제 : shift()
+members.shift();
+console.log(members);
+//배열의 마지막 요소를 삭제 : pop()
+members.pop();
+console.log(members);
+// 배열의 첫 요소로 값 추가 : unshift(value)
+members.unshift('NiceCodeit');
+console.log(members);
+// 배열의 마지막 요소로 값 추가 : push(value)
+members.push('HiCodeit');
+console.log(members);
