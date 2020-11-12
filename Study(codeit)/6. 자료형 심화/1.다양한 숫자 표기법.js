08bd48073dc748842ff5c971c378e6e329a9b5b7
JaeHyun <soehd0889@naver.com>
2020-11-12 23:32:28 +0900

    11-12 1.다양한 숫자 표기법

---
+++ Study(codeit)/6. 자료형 심화/1.다양한 숫자 표기법.js
@@ -0,0 +1,24 @@
+//숫자 표기법 
+let millionaire = 1000000000;
+let myNumber = 1e9;
+
+console.log(millionaire);
+console.log(myNumber);
+console.log(millionaire === myNumber);
+
+console.log(25e5 === 2500000);
+console.log(5.3e3 === 5300);
+console.log(-6.1e8 === -610000000);
+
+//16진법
+let hex1 = 0xff;
+let hex2 = 0xFF;
+// 8 진법
+let octal = 0o377;
+//2진법
+let binary = 0b11111111;
+
+console.log(hex1);
+console.log(hex2);
+console.log(octal);
+console.log(binary);
