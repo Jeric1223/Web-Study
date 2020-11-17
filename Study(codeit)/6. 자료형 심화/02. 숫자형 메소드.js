e03f143aeb2a3c7ed385e936c1cdfa6e1f2529b1
JaeHyun <soehd0889@naver.com>
2020-11-17 19:18:44 +0900

    11-17 숫자형 메소드(JS)

---
+++ Study(codeit)/6. 자료형 심화/02. 숫자형 메소드.js
@@ -0,0 +1,10 @@
+//Number
+let myNumber = 0.3567;
+
+//toFixed(0~100)
+//반올림 함수
+console.log(myNumber.toFixed(1)); //출력 타입은 string이다 숫자로 하려면 Number(코드를 작성),+으로 나타낸다. 
+ // 숫자로 사용하면 123..toFixed로 하면 된다/
+
+// toString(2 ~ 36) n진수로 변환
+console.log(myNumber.toString(5));
