cc886ee1963b11cb204ecd930e761f739a129ab7
JaeHyun <soehd0889@naver.com>
2020-12-31 08:34:21 +0900

    12-31 web js 연습

--- WEB JS/index.js
+++ WEB JS/index.js
@@ -1,13 +1,14 @@
-const title = document.getElementById("background");
+const background = document.getElementById("background");
 var circle = document.getElementById('circle');
 var circle2 = document.getElementById('circle2');
+const wifi = document.getElementById("wifi");
 
 
 function haveResize(){
   var fuckcolor = "#" + Math.round(Math.random() * 0xffffff).toString(16);
   document.body.style.backgroundColor = fuckcolor;
   document.body.style.transition = '0.5s';
-  title.innerHTML = fuckcolor;
+  background.innerHTML = fuckcolor;
 }
 function circleClick(){
   var fuckcolor2 = "#" + Math.round(Math.random() * 0xffffff).toString(16);
@@ -15,15 +16,15 @@ function circleClick(){
   circle2.innerHTML = fuckcolor2;
   circle.style.transition = '0.5s';
 }
-/* function moveCircle(e) {
-  TweenLite.to('circle', 0.3, {
-    css: {
-      left: e.pageX,
-      top: e.pageY
-    }
-  });
-} */
+function offlinePage(){
+ wifi.innerHTML = "와이파이가 꺼졌습니다";
+}
+function onlinePage(){
+  wifi.innerHTML = "네트워크 연결되었습니다.";
+}
 
+  window.addEventListener('online', offlinePage);
+  window.addEventListener('offline', offlinePage);
 window.addEventListener('click', haveResize);
 circle.addEventListener('click', circleClick);
 
