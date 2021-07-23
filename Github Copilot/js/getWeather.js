cf448acc13fb0834f88b00c3b402d4cb0d36cb50
JaeHyun <soehd0889@naver.com>
2021-07-23 17:02:00 +0900

    Github Copilot 사용

--- Github Copilot/js/getWeather.js
+++ Github Copilot/js/getWeather.js
@@ -123,7 +123,7 @@ function getWeatherKr() {
 
 getWeatherKr();
 
-// github에 hoohoo0889사용자 커밋수 출력
+/* // github에 hoohoo0889사용자 커밋수 출력
 function getCommit() {
   var url =
     "https://api.github.com/repos/hoohoo0889/web-development-with-jquery/commits";
@@ -142,4 +142,69 @@ function getCommit() {
   xhr.send(null);
 }
 
-getCommit();
+getCommit(); */
+
+//깃허브 hoohoo0889 정보 출력
+function getGithub() {
+  var url = "https://api.github.com/users/hoohoo0889/repos";
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", url, true);
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState == 4) {
+      if (xhr.status == 200) {
+        var json = JSON.parse(xhr.responseText);
+        var repos = json.length;
+        document.getElementById("repos").innerHTML =
+          "hoohoo0889님의 깃허브에 저장된 프로젝트의 수는 " +
+          repos +
+          "개 입니다.";
+      }
+    }
+  };
+  xhr.send(null);
+}
+
+getGithub();
+
+//네이버 주식 가격 출력
+function getStock() {
+  var url = "https://finance.naver.com/item/main.nhn?code=005930";
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", url, true);
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState == 4) {
+      if (xhr.status == 200) {
+        var json = JSON.parse(xhr.responseText);
+        var price = json.data.item.price;
+        document.getElementById("stock").innerHTML =
+          "네이버 주식 가격은 " + price + "입니다.";
+      }
+    }
+  };
+  xhr.send(null);
+}
+
+getStock();
+
+//네이버 실시간 검색어 출력
+function getRealtime() {
+  var url = "https://finance.naver.com/";
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", url, true);
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState == 4) {
+      if (xhr.status == 200) {
+        var json = JSON.parse(xhr.responseText);
+        var title = json.title;
+        var realtime = json.realtime;
+        document.getElementById("realtime").innerHTML =
+          "네이버 실시간 검색어는 " + title + "입니다.";
+        document.getElementById("realtime2").innerHTML =
+          "네이버 실시간 검색어는 " + realtime + "입니다.";
+      }
+    }
+  };
+  xhr.send(null);
+}
+
+getRealtime();
